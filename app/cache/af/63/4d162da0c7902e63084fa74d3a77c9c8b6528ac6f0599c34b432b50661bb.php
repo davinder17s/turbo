1581e45821c5815fd09ce1f7ea4a730822982987
Davinder <davinder17s@gmail.com>
2014-10-13 10:14:43 +0530

    Empty folders fix

---
+++ app/cache/af/63/4d162da0c7902e63084fa74d3a77c9c8b6528ac6f0599c34b432b50661bb.php
@@ -0,0 +1,29 @@
+<?php
+
+/* home.twig */
+class __TwigTemplate_af634d162da0c7902e63084fa74d3a77c9c8b6528ac6f0599c34b432b50661bb extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "home.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+}
